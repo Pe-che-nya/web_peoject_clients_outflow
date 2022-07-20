b85b5696ab1beaca22330f95c1a9841de3281669
Alexey <Alexey@Yandex.ru>
2022-07-20 23:31:57 +0400

    create html and CSS

--- clients_outflow/urls.py
+++ clients_outflow/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import predict.views as predictviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('listClients/', predictviews.main)
 ]
--- predict/views.py
+++ predict/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+navigation = [{
+    'title': 'Информация',
+    'link': '#'
+},{
+    'title': 'Учетка пользователя',
+    'link': '#'
+}]
+def main(request):
+    context = {'navigation': navigation}
+    return render(request, 'listClients.html', context=context)
 # Create your views here.
